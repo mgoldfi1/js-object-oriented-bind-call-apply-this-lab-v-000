f0f5f016ab541b26175f8ecdc8be43b7e85820a4
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 21:26:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args)
 }
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const func = functionToBeCopied.bind(thisValue)
+  return func
+}
